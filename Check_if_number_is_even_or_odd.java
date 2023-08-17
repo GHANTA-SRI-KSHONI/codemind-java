cee4b607102632ed28ad316e14615efa35fa2ff7
GHANTA-SRI-KSHONI <119730294+GHANTA-SRI-KSHONI@users.noreply.github.com>
2023-08-17 11:26:08 +0530

    import java.util.Scanner; public class Kshoni {    public static void main(String args[])     {
          Scanner s= new Scanner(System.in);
          int n=s.nextInt();
          if(n%2==0)
          {
              System.out.println("Even");
          }
          else
          {
              System.out.println("Odd");
          }
        }     
    }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Kshoni
+{
+   public static void main(String args[]) 
+    {   
+       
+      Scanner s= new Scanner(System.in);
+      int n=s.nextInt();
+      if(n%2==0)
+      {
+          System.out.println("Even");
+      }
+      else
+      {
+          System.out.println("Odd");
+      }
+    }     
+}
