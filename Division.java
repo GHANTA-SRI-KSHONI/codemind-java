35f445590c4a5928a0433bd8d2ecf10a4bcf3916
GHANTA-SRI-KSHONI <119730294+GHANTA-SRI-KSHONI@users.noreply.github.com>
2023-08-25 11:09:28 +0530

    import java.util.Scanner; public class Siri{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int div=(int)(a/b);         System.out.format("%d",div);
        }
    }

---
+++ Division.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Siri{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int div=(int)(a/b);
+        System.out.format("%d",div);
+    
+    }
+}
